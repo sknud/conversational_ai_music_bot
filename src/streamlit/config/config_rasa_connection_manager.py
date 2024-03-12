2bbdd3f351cb5d3b23bace4e1c7dd99ded5d40fe
dougalk <dougal.knight@sky.uk>
2024-03-12 15:48:13 +0000

    removing commented out code

--- src/streamlit/config/config_rasa_connection_manager.py
+++ src/streamlit/config/config_rasa_connection_manager.py
@@ -1,20 +1,6 @@
 import json
 import os
 
-# # Load the rasa endpoint
-# def load_rasa_endpoint(config_file_path, use_local=True):
-#     try:
-#         with open(config_file_path, encoding="utf-8") as config_file:
-#             config_data = json.load(config_file)
-#         if use_local:
-#             rasa_endpoint = config_data['url_local']
-#         else:
-#             rasa_endpoint = config_data['url_aws']
-#         return rasa_endpoint
-#     except (FileNotFoundError, KeyError, json.JSONDecodeError) as e:
-#         print(f"Error: {e}")
-#         return None
-
 # Load the rasa endpoint
 def load_rasa_endpoint(config_file_path, use_local=True):
     try:
